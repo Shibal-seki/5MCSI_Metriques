ebde2a050c0060ef359da0f3980dd26b817c9022
Shibal-seki <dwaynevallade97293@gmail.com>
2025-01-09 09:56:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,15 +7,16 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
   
-  @app.route('/tawarano/')
+@app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
